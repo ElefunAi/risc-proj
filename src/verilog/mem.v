f3805768e27f02f49a941d4eefb6556027ed17b8
ElefunAi <rfunivstudy@gmail.com>
2022-05-29 12:14:46 +0900

    プログラム記述用hexディレクトリの追加と hexdata読み書きようのメモリモジュールの記述

---
+++ src/verilog/mem.v
@@ -0,0 +1,25 @@
+// memoryモジュール
+module mem (
+    input wire clk,
+    input wire write_en,
+    input wire [31:0] addr,
+    input wire [31:0] write_data,
+    output wire [31:0] read_data
+);
+    // 4byte*4096行=16384byte=16KB
+    reg [31:0] rom [4096];
+    reg [31:0] read_reg;
+
+    initial begin
+        $readmemh("../hex/data.hex", rom);
+    end
+    
+    always @(posedge clk) begin
+        read_reg <= rom[addr];
+        if (write_en) begin
+            rom[addr] <= write_data;
+        end
+    end
+
+    assign read_data = read_reg;
+endmodule
