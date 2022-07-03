d2db9417f50c5357938d5a3aefc1c34ed2818514
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2022-07-03 13:03:54 +0900

    プログラムカウンタの修正

--- src/verilog/pc.v
+++ src/verilog/pc.v
@@ -1,11 +1,12 @@
 module PC (
+    input wire clk,
     input wire jump_flag,
     input wire [31:0] jump_target,
     output wire [31:0] pc
 );
     reg [31:1] pc_reg;
 
-    always @(*) begin
+    always @(posedge clk) begin
         pc_reg <= (jump_flag) ? jump_target : pc + 4;
     end
 
