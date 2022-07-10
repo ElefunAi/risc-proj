4c8f5304d49bdd7e264989c73be48be340e789e9
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2022-07-10 11:27:32 +0900

    jump_controllerを修正

--- src/verilog/jump_controller.v
+++ src/verilog/jump_controller.v
@@ -9,23 +9,23 @@ module jump_controller (
     reg [31:0] jump_target_reg,
 
     // 条件分岐
-    wire [31:0] beq_out;
-    assign beq_out = (rs1_data == rs2_data) ? 32'b1 : 32'b0;
+    wire beq_out;
+    assign beq_out = (rs1_data == rs2_data) ? 1'b1 : 1'b0;
 
-    wire [31:0] bne_out;
-    assign bne_out = (rs1_data != rs2_data) ? 32'b1 : 32'b0;
+    wire bne_out;
+    assign bne_out = (rs1_data != rs2_data) ? 1'b1 : 1'b0;
 
-    wire [31:0] blt_out;
-    assign blt_out = ($signed(rs1_data) < $signed(rs2_data)) ? 32'b1 : 32'b0;
+    wire blt_out;
+    assign blt_out = ($signed(rs1_data) < $signed(rs2_data)) ? 1'b1 : 1'b0;
 
-    wire [31:0] bge_out;
-    assign bge_out = ($signed(rs1_data) > $signed(rs2_data)) ? 32'b1 : 32'b0;
+    wire bge_out;
+    assign bge_out = ($signed(rs1_data) > $signed(rs2_data)) ? 1'b1 : 1'b0;
     
-    wire [31:0] bltu_out;
-    assign bltu_out = (rs1_data < rs2_data) ? 32'b1 : 32'b0;
+    wire bltu_out;
+    assign bltu_out = (rs1_data < rs2_data) ? 1'b1 : 1'b0;
 
-    wire [31:0] bgeu_out;
-    assign bgeu_out = (rs1_data > rs2_data) ? 32'b1 : 32'b0;
+    wire bgeu_out;
+    assign bgeu_out = (rs1_data > rs2_data) ? 1'b1 : 1'b0;
 
     wire [31:0] jalr_out;
     assign jalr_out = (rs1_data + rs2_data) & ~32'b1;
@@ -38,7 +38,7 @@ module jump_controller (
             `BR_BGE   : jump_flag <= bge_out;
             `BR_BLTU  : jump_flag <= bltu_out;
             `BR_BGEU  : jump_flag <= bgeu_out;
-            default   : jump_flag <= 0;
+            default   : jump_flag <= 1'b0;
         endcase
         case (exe_fn)
             `ALU_JALR : jump_target_reg <= jalr_out;
