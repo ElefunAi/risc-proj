e76cc1046ef01628e46474f6821e39f3fc8bf921
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2022-07-10 10:40:28 +0900

    CPU.v内の下地を作成

--- src/verilog/CPU.v
+++ src/verilog/CPU.v
@@ -1,3 +1,38 @@
 module CPU;
-    initial $display (3'd6 + 3'd1);
+    wire [31:0] ;
+    wire ;
+
+    decoder decoder (
+        .inst(),
+        .op1_data(),
+        .op2_data(),
+        .exe_fun(),
+        .op1(),
+        .mem_wen(),
+        .op2(),
+        .wb_sel(),
+        .rf_wen()
+    );
+
+    ALU ALU (
+        .alu_fn(),
+        .rs1_data(),
+        .rs2_data(),
+        .out()
+    );
+
+    PC PC (
+        .clk(),
+        .jump_flag(),
+        .jump_target(),
+        .pc()
+    );
+
+    mem mem (
+    .clk(),
+    .write_en(),
+    .addr(),
+    .write_data(),
+    .read_data()
+);
 endmodule
