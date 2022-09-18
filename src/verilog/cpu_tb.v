a13eddc6d40a0a33ba3c7dce8504025050627191
ElefunAi <rfunivstudy@gmail.com>
2022-09-18 12:44:37 +0900

    for debug

--- src/verilog/cpu_tb.v
+++ src/verilog/cpu_tb.v
@@ -36,7 +36,7 @@ parameter CYCLE = 1;
       // $fdisplay(FP, "%4d: %h", idx, read_data);
     // end
     #CYCLE reset = 1;
-    $fmonitor(FP, "%4dns clock=%d pc=%d inst=%h rs1_addr=%d rs1_data=%d rs2_addr=%d rs2_data=%d", $stime, clk, riscv.pc, riscv.inst_out, riscv.op1_addr, riscv.rs1_data, riscv.op2_addr, riscv.rs2_data);
+    $monitor(FP, "%4dus clock=%d pc=%d inst=%h rs1_addr=%d rs1_data=%d rs2_addr=%d rs2_data=%d reg_write_addr=%d reg_write_val=%d", $stime, clk, riscv.pc, riscv.inst_out, riscv.op1_addr, riscv.rs1_data, riscv.op2_addr, riscv.rs2_data, riscv.reg_write_addr, riscv.reg_write_value);
 
     // リセット信号は代入されないので、手動で下げる
     #CYCLE     reset = 0;
